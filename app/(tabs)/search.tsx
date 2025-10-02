0129bb100d1bca3037793ea86f7f7b7d4a84dc78
Aman Chaurasiya <chaurasiyaaman741@gmail.com>
2025-10-03 04:25:36 +0530

    Initial commit of react native Learning

---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const search = () => {
+  return (
+    <View
+      className="flex-1 justify-center items-center"
+    >
+      <Text className="text-5xl text-dark-200 font-bold">Search !</Text>
+    </View>
+  )
+}
+
+export default search
---
+++ app/movies/[id].tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const MoviDetails = () => {
+  return (
+    <View>
+      <Text>MoviDetails</Text>
+    </View>
+  )
+}
+
+export default MoviDetails
